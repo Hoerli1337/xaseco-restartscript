fb4d970f966cd2458ee53b4fdb95a5b50cfd71bd
Hoerli <13108516+Hoerli1337@users.noreply.github.com>
2020-02-02 13:58:26 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,8 +1,7 @@
 if ps -p $(cat pid.txt) > /dev/null 2>&1
 then
-    echo uaseco is running!
+    echo xaseco is running!
 else
     rm pid.txt
-    #./webrequset.sh
-    ./uaseco.sh
+    ./Aseco.sh
 fi
